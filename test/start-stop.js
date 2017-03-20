e82fd874d29fc053e5b07a4995bf27dfadcb80a3
Adam Juhasz <adam@akitram.com>
2017-03-20 18:17:30 -0400

    more testing

---
+++ test/start-stop.js
@@ -0,0 +1,32 @@
+const BotTester = require('messenger-bot-tester');
+const Alana = require('@alana/core');
+const FB = require('../lib/index').default;
+const request = require('request-promise');
+
+describe('start & stop', function() {
+  // webHookURL points to where yout bot is currently listening
+  // choose a port for the test framework to listen on
+  const testingPort = Math.round(Math.random()*1000) + 4000;
+  const botPort = Math.round(Math.random()*1000) + 4000;
+  const webHookURL = 'http://localhost:' + botPort + '/webhook';
+  const tester = new BotTester.default(testingPort, webHookURL);
+  process.env.FB_GRAPHURLBASE = `http://localhost:${testingPort}`;
+  const bot = new Alana.default();
+  bot.turnOnDebug();
+  const platform = new FB(bot, botPort, 'access_token', '/webhook', 'verify_me');
+
+  before(function(){
+    // start your own bot here or having it running already in the background
+    // redirect all Facebook Requests to http://localhost:3100/v2.6 and not https://graph.facebook.com/v2.6
+    bot.start();
+    return tester.startListening();
+  });
+  
+  it('start', function(){
+    return bot.start();
+  });
+
+  it('stop', function(){
+    return bot.stop();
+  });
+})
---
+++ test/verify.js
@@ -0,0 +1,58 @@
+const BotTester = require('messenger-bot-tester');
+const Alana = require('@alana/core');
+const FB = require('../lib/index').default;
+const request = require('request-promise');
+
+describe('verify', function() {
+  // webHookURL points to where yout bot is currently listening
+  // choose a port for the test framework to listen on
+  const testingPort = Math.round(Math.random()*1000) + 4000;
+  const botPort = Math.round(Math.random()*1000) + 4000;
+  const webHookURL = 'http://localhost:' + botPort + '/webhook';
+  const tester = new BotTester.default(testingPort, webHookURL);
+  process.env.FB_GRAPHURLBASE = `http://localhost:${testingPort}`;
+  const bot = new Alana.default();
+  bot.turnOnDebug();
+  const platform = new FB(bot, botPort, 'access_token', '/webhook', 'verify_me');
+
+  before(function(){
+    // start your own bot here or having it running already in the background
+    // redirect all Facebook Requests to http://localhost:3100/v2.6 and not https://graph.facebook.com/v2.6
+    bot.start();
+    return tester.startListening();
+  });
+  
+  it('good token', function(){
+    return request({
+      uri: `http://localhost:${botPort}/webhook`,
+      method: 'GET',
+      qs: {
+        'hub.verify_token': 'verify_me',
+        'hub.challenge': 'challenge',
+      },
+    })
+    .then(response => {
+      console.log(response);
+      if (response !== 'challenge') {
+        throw new Error('bad response');
+      }
+    })
+  });
+
+  it('bad token', function(){
+    return request({
+      uri: `http://localhost:${botPort}/webhook`,
+      method: 'GET',
+      qs: {
+        'hub.verify_token': 'verify_them',
+        'hub.challenge': 'challenge',
+      },
+    })
+    .then(response => {
+      console.log(response);
+      if (response !== 'Error, wrong validation token') {
+        throw new Error('should be bad response');
+      }
+    })
+  });
+})
